0d9c4b3a5a73e0a221416cd0fed04fb5bf0e6bd2
Sophia Duncan <20Sophia.Duncan@boiseschools.net>
2019-08-22 13:09:35 -0600

    Martinetti game project

---
+++ DieGame.py
@@ -0,0 +1,56 @@
+import random
+class Die :
+    def __init__(self, dieSides = 6) :
+        self.dieNum = 0
+
+    def __str__ ( self ) :
+        return dieNum
+
+    def random ( self ) :
+        dieNum = random.randint(1,6)
+
+
+class Testing :
+    def __init__(self, dieOne)
+        test = Testing(Die())
+
+    def __str__ (self)
+
+#test = Testing(Die())
+
+
+class Martinetti :
+    def __init__ (self, dieFirst, dieSecond)
+        self.dieOne = dieFirst 
+        self.dieTwo = dieSecond
+        self.positionOne = [1,2,3,4,5,6,7,8,9,10,11,12] #Marks position going up
+        self.positionTwo = [12,11,10,9,8,7,6,5,4,3,2,1] #Marks position going down
+        #0 will mean that a position has been reached
+
+    #Rolls the dice
+    def roll (self) 
+        dieOne.random()
+        dieTwo.random()
+
+    # Checks and changes the position in the game (what numbers are crossed off)
+    def positions (self)
+        roll()
+        oneEmpty = false
+        twoEmpty = false
+        num = 0
+
+        for i in xrange(0,11, +1) :
+            if positionOne
+            
+        
+
+    
+    
+
+
+
+        
+
+    
+
+    
